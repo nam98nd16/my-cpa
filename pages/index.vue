193b8651ce62e4cd6be147fade375ed51c57485a
nam <nam98nd16@gmail.com>
2020-07-18 14:50:42 +0700

    init table

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,82 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        my-cpa
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div style="display: flex;
+  justify-content: center;">
+    <hotTable
+      :data="dataSource"
+      :rowHeaders="false"
+      :allowRemoveRow="true"
+      :colHeaders="['Semester', 'Course ID', 'Course Title', 'Credit', 'Grade']"
+      licenseKey="non-commercial-and-evaluation"
+    ></hotTable>
+    <div>CPA: {{cpa.toFixed(2)}} over {{totalCredits}} credits</div>
   </div>
 </template>
 
 <script>
-export default {}
+import { HotTable } from "@handsontable/vue";
+import { allCourses } from "../utils/const";
+
+export default {
+  data() {
+    return {
+      dataSource: [],
+      allCourses,
+      totalCredits: 0
+    };
+  },
+  components: {
+    HotTable
+  },
+  computed: {
+    cpa() {
+      let cpa = 0;
+      let totalGrade = 0;
+      this.totalCredits = 0;
+      let handleCourseGrade = (grade, courseCredit) => {
+        totalGrade += grade * courseCredit;
+        this.totalCredits +=
+          typeof courseCredit === "number"
+            ? courseCredit
+            : parseInt(courseCredit);
+      };
+      this.dataSource.forEach(course => {
+        if (["A", "A+", "a", "a+"].includes(course.courseGrade))
+          handleCourseGrade(4, course.courseCredit);
+        else if (["B+", "b+"].includes(course.courseGrade))
+          handleCourseGrade(3.5, course.courseCredit);
+        else if (["B", "b"].includes(course.courseGrade))
+          handleCourseGrade(3, course.courseCredit);
+        else if (["C+", "c+"].includes(course.courseGrade))
+          handleCourseGrade(2.5, course.courseCredit);
+        else if (["C", "c"].includes(course.courseGrade))
+          handleCourseGrade(2, course.courseCredit);
+        else if (["D+", "d+"].includes(course.courseGrade))
+          handleCourseGrade(1.5, course.courseCredit);
+        else if (["D", "d"].includes(course.courseGrade))
+          handleCourseGrade(1, course.courseCredit);
+        else if (["F", "f"].includes(course.courseGrade))
+          handleCourseGrade(0, course.courseCredit);
+      });
+      cpa = totalGrade / this.totalCredits;
+      return cpa;
+    }
+  },
+  mounted() {
+    let i = 0;
+    for (let course in this.allCourses) {
+      let trueCourse = {
+        semester: this.allCourses[course][0],
+        courseId: this.allCourses[course][1],
+        courseTitle: this.allCourses[course][2],
+        courseCredit: this.allCourses[course][3],
+        courseGrade: this.allCourses[course][4]
+      };
+      this.$set(this.dataSource, i, trueCourse);
+      i++;
+    }
+  },
+  methods: {}
+};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style src="../node_modules/handsontable/dist/handsontable.full.css"></style>
---
+++ utils/const.js
@@ -0,0 +1,156 @@
+let IT4682E = ["20192", "IT4682E", "Multimedia Communication", 2, "A"];
+let IT4612E = ["20192", "IT4612E", "Distributed System", 2, "B+"];
+let IT4552E = ["20192", "IT4552E", "Web Programming", 2, "A"];
+let IT4945E = ["20191", "IT4945E", "ITSS Software Development", 2, "B"];
+let IT4592E = ["20191", "IT4592E", "Information Theory", 2, "A"];
+let IT4492E = ["20191", "IT4492E", "Structured Programming", 2, "A"];
+let IT4272E = ["20191", "IT4272E", "Computer Systems", 2, "B"];
+let IT4182E = ["20191", "IT4182E", "Compiler Construction Lab", 2, "A"];
+let IT4142E = ["20191", "IT4142E", "Introduction to Data Science", 2, "B+"];
+let IT4012E = ["20191", "IT4012E", "Information Security", 2, "B"];
+let IT3322E = ["20191", "IT3322E", "Compiler Construction", 2, "A"];
+let IT4948 = ["20183", "IT4948", "Thực tập công nghiệp", 2, "A"];
+let FL1022 = ["20183", "FL1022", "Kỹ năng tổng hợp I", 2, "A"];
+let FL1018 = ["20183", "FL1018", "Kỹ năng đọc TA I", 2, "A"];
+let FL1017 = ["20183", "FL1017", "Kỹ năng nghe TA I", 2, "A"];
+let PE2402 = ["20182", "PE2402", "Bóng bàn 2", 0, "D+"];
+let IT4082E = ["20182", "IT4082E", "Software Engineering", 2, "A"];
+let IT4042E = ["20182", "IT4042E", "Artificial Intelligence", 2, "B"];
+let IT3292E = ["20182", "IT3292E", "Database", 2, "B"];
+let IT3290E = ["20182", "IT3290E", "Database Lab", 2, "A"];
+let IT3282E = ["20182", "IT3282E", "Computer Architecture", 2, "B"];
+let IT3280E = [
+  "20182",
+  "IT3280E",
+  "Assembly Language and Computer Architecture Lab",
+  2,
+  "A"
+];
+let IT3102E = [
+  "20182",
+  "IT3102E",
+  "Object-Oriented Language and Theory (Java)",
+  3,
+  "B"
+];
+let IT3082E = ["20182", "IT3082E", "Computer Network", 2, "B+"];
+let IT3072E = ["20182", "IT3072E", "Operating Systems", 2, "B"];
+let MI2022 = ["20181", "MI2022", "Probability Theory", 2, "B"];
+let MI1042 = ["20181", "MI1042", "Math IV", 3, "C"];
+let IT3270E = ["20181", "IT3270E", "Logic Circuit Lab", 2, "B"];
+let IT3260E = ["20181", "IT3260E", "Logic Circuit", 2, "C+"];
+let IT3250E = ["20181", "IT3250E", "Computer Ethics", 2, "C"];
+let IT3240E = [
+  "20181",
+  "IT3240E",
+  "Data Structures and Algorithms Advanced Lab",
+  2,
+  "A"
+];
+let IT3022E = ["20181", "IT3022E", "Discrete Math", 2, "B+"];
+let EE2018 = ["20181", "EE2018", "Electrical Engineering", 3, "B+"];
+let SSH1130 = ["20172", "SSH1130", "Đường lối CM của ĐCSVN", 3, "B+"];
+let PH1028 = ["20172", "PH1028", "Physics II", 4, "D"];
+let PE2017 = ["20172", "PE2017", "Bóng bàn I", 0, "B+"];
+let MI1032 = ["20172", "MI1032", "Math III", 3, "C"];
+let IT3312E = ["20172", "IT3312E", "Data Structures & Algorithms", 2, "C+"];
+let IT3230E = [
+  "20172",
+  "IT3230E",
+  "Data Structures and Algorithms Basic Lab",
+  2,
+  "A"
+];
+let ET2015 = ["20172", "ET2015", "Electronics", 4, "C"];
+let SSH1050 = ["20171", "SSH1050", "Tư tưởng HCM", 2, "C"];
+let PH1018 = ["20171", "PH1018", "Physics I", 4, "C"];
+let PE1030 = ["20171", "PE1030", "Giáo dục thể chất C", 0, "C"];
+let MIL1130 = ["20171", "MIL1130", "QS chung và KCT bắn súng AK", 0, "B"];
+let MI1022 = ["20171", "MI1022", "Math II", 3, "C+"];
+let MI1012 = ["20171", "MI1012", "Math I", 3, "D"];
+let IT3220 = ["20171", "IT3220", "C Programming (Introduction)", 2, "B"];
+let IT3210 = ["20171", "IT3210", "C Programming Language", 2, "B"];
+let FL2016 = ["20171", "FL2016", "Kỹ năng viết TA III", 2, "A"];
+let SSH1120 = ["20162", "SSH1120", "Những NLCB của CNML II", 3, "B"];
+let PE1020 = ["20162", "PE1020", "Giáo dục thể chất B", 0, "D+"];
+let MIL1120 = ["20162", "MIL1120", "Công tác quốc phòng-An ninh", 0, "C+"];
+let IT2120 = ["20162", "IT2120", "Kiến thức máy tính", 2, "A"];
+let IT2110 = ["20162", "IT2110", "Nhập môn CNTT và TT", 2, "C+"];
+let FL1031 = ["20162", "FL1031", "Kỹ năng tổng hợp II", 2, "B"];
+let FL1029 = ["20162", "FL1029", "Kỹ năng viết TA II", 2, "C+"];
+let FL1028 = ["20162", "FL1028", "Kỹ năng đọc TA II", 2, "A"];
+let FL1027 = ["20162", "FL1027", "Kỹ năng nghe TA II", 2, "B"];
+let FL1026 = ["20162", "FL1026", "Kỹ năng nói TA II", 2, "A"];
+let SSH1110 = ["20161", "SSH1110", "Những NLCB của CNML I", 2, "D"];
+let PE1010 = ["20161", "PE1010", "Giáo dục thể chất A", 0, "B+"];
+let MIL1110 = ["20161", "MIL1110", "Đường lối quân sự", 0, "B"];
+let FL1019 = ["20161", "FL1019", "Kỹ năng viết TA I", 2, "B"];
+let FL1016 = ["20161", "FL1016", "Kỹ năng nói TA I", 2, "B"];
+let EM1170 = ["20161", "EM1170", "Pháp luật đại cương", 2, "C+"];
+export const allCourses = {
+  IT4682E: IT4682E,
+  IT4612E: IT4612E,
+  IT4552E: IT4552E,
+  IT4945E: IT4945E,
+  IT4592E: IT4592E,
+  IT4492E: IT4492E,
+  IT4272E: IT4272E,
+  IT4182E: IT4182E,
+  IT4142E: IT4142E,
+  IT4012E: IT4012E,
+  IT3322E: IT3322E,
+  IT4948: IT4948,
+  FL1022: FL1022,
+  FL1018: FL1018,
+  FL1017: FL1017,
+  PE2402: PE2402,
+  IT4082E: IT4082E,
+  IT4042E: IT4042E,
+  IT3292E: IT3292E,
+  IT3290E: IT3290E,
+  IT3282E: IT3282E,
+  IT3280E: IT3280E,
+  IT3102E: IT3102E,
+  IT3082E: IT3082E,
+  IT3072E: IT3072E,
+  MI2022: MI2022,
+  MI1042: MI1042,
+  IT3270E: IT3270E,
+  IT3260E: IT3260E,
+  IT3250E: IT3250E,
+  IT3240E: IT3240E,
+  IT3022E: IT3022E,
+  EE2018: EE2018,
+  SSH1130: SSH1130,
+  PH1028: PH1028,
+  PE2017: PE2017,
+  MI1032: MI1032,
+  IT3312E: IT3312E,
+  IT3230E: IT3230E,
+  ET2015: ET2015,
+  SSH1050: SSH1050,
+  PH1018: PH1018,
+  PE1030: PE1030,
+  MIL1130: MIL1130,
+  MI1022: MI1022,
+  MI1012: MI1012,
+  IT3220: IT3220,
+  IT3210: IT3210,
+  FL2016: FL2016,
+  SSH1120: SSH1120,
+  PE1020: PE1020,
+  MIL1120: MIL1120,
+  IT2120: IT2120,
+  IT2110: IT2110,
+  FL1031: FL1031,
+  FL1029: FL1029,
+  FL1028: FL1028,
+  FL1027: FL1027,
+  FL1026: FL1026,
+  SSH1110: SSH1110,
+  PE1010: PE1010,
+  MIL1110: MIL1110,
+  FL1019: FL1019,
+  FL1016: FL1016,
+  EM1170: EM1170
+};
